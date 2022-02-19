580a6731c9603f9b75da16f664a0c39c5ac61c92
6kq <65254157+6kq@users.noreply.github.com>
2022-02-19 11:34:08 +0100

    Update Program.cs

--- CL_Anti_Public_Checker/Program.cs
+++ CL_Anti_Public_Checker/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +12,7 @@ namespace CL_Anti_Public_Checker
     {
         static void Main(string[] args)
         {
+            if(!File.Exists("data.txt")) File.Create("data.txt");
             Console.Title = Util.RequestUtil.getHashesInDB() + " hashes in db";
             foreach(string p in File.ReadAllLines("data.txt"))
             {
